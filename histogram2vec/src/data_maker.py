1c4cb8a9b626869b300b35dfa2e3535841a7ca68
tadahayamiz <tadahaya@gmail.com>
2023-09-18 16:45:49 +0900

    update

--- histogram2vec/src/data_maker.py
+++ histogram2vec/src/data_maker.py
@@ -14,7 +14,7 @@ import os
 import matplotlib.pyplot as plt
 from collections import Counter
 from itertools import chain
-from tqdm import tqdm, trange
+from tqdm.auto import tqdm, trange
 
 
 FIELD = [
